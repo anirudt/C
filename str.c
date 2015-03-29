5bc24389bb97e7ab644fffd8df3fe2c14691aaca
Anirud <anirudt@gmail.com>
2015-03-29 11:42:46 +0530

    added string file

---
+++ str.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char *a[7];
+	a[0] = "Monday";
+	a[1] = "Tuesday";
+	a[2] = "Wednesday";
+	a[3] = "Thursday";
+	printf("%s\n", a[3]);
+	return 0;
+}
